b7531cccb8a93f8adf8e4f724151f9e801912e39
Krishna Sharma <krishnakumar16aug@hotmail.com>
2022-05-17 18:54:23 +0100

    added shell files

---
+++ runAnalyser.ps1
@@ -0,0 +1,10 @@
+#run python classfier for calssification
+python3 -m classfier
+
+#making the directories for normal/abnormal plotting
+mkdir Graph
+mkdir Graph/Abnormal
+mkdir Graph/Normal
+
+#Running the Linear programming and plotting the data
+python3 -m lpGeneratorPlot
