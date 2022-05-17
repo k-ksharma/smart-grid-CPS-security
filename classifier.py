9e1cde30cffbd78652c380de7c593631794153a8
Krishna Sharma <krishnakumar16aug@hotmail.com>
2022-05-17 17:19:23 +0100

    updated classifier from algorithmic to scikit library

--- classifier.py
+++ classifier.py
@@ -1,240 +1,73 @@
-from collections import Counter
-import sys, os, math
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn import linear_model
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 
+#feeding training dataset
+trainDS = pd.read_csv('TrainingData.txt', header=None)
+y = trainDS[24].tolist()
+trainDS = trainDS.drop(24, axis=1)
+x = trainDS.values.tolist()
 
-PETHOLD = 5 ##suspicions threshold
-OHHOLD = 53 ##confidence threshold
+#storing input before splitting
+x = np.array(x)
+y = np.array(y)
+xTrainFull = x
+yTrainFull = y
 
-#getters
-def getPETHOLD():
-	global PETHOLD
-	return PETHOLD
+#feeding test data
+testDS = pd.read_csv('TestingData.txt', header=None)
+xClassify = testDS.values.tolist()
 
-#setters
-def setPETHOLD(value=5):
-	global PETHOLD
-	PETHOLD = value
+#splitting training dataset for testing
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2, random_state=1004)
 
-#getters
-def getOHHOLD():
-	global OHHOLD
-	return OHHOLD
+#normalising by scaling between 0 and 1
+scaler = MinMaxScaler()
+xTrain = scaler.fit_transform(xTrain)
+xTest = scaler.transform(xTest)
+xClassify = scaler.transform(xClassify)
+xTrainFull = scaler.transform(xTrainFull)
+''''
 
-#setters
-def setOHHOLD(value=50):
-	global OHHOLD
-	OHHOLD = value
+##loop to find the n-neighbours value - 
+plt.rcParams["figure.figsize"] = (20,10)
+train_accuracy = []
+test_accuracy = []
+n_neighbors = range(1, 15)
 
+for n in n_neighbors :
+    knn = KNeighborsClassifier(n_neighbors = n)
+    knn.fit(xTrain, yTrain)
+    train_accuracy.append(knn.score(xTrain, yTrain))
+    test_accuracy.append(knn.score(xTest, yTest))
+     
+plt.plot(n_neighbors, train_accuracy, label = 'train_accuracy')
+plt.plot(n_neighbors, test_accuracy, label = 'test_accuracy')
+plt.xlabel("k", fontsize = 15)
+plt.ylabel("accuracy", fontsize = 15)
+plt.show()
 
-#parsing filename
-def _parseFile(fileName):
-	if(os.path.splitext(fileName)[1] != ".txt"): return
-	data = []
-	with open(fileName, mode='r', encoding='utf-8') as f:
-		for line in f:
-			vec = _parseLine(line)
-			if(len(vec)>=24):
-				data.append(vec)
-	return data
+##command to check the accuracy, once we have selected an optimal value of number of n neighbours
 
-def _parseLine(line):
-	if(line == "" or line == " "): return []
-	v = list(line.split(","))
-	if(len(v) == 24 or len(v) == 25): return v
-
-def _chooseK(data):
-	k = math.floor(math.sqrt(len(data)))
-	if(k%2==0): k+=1
-	return k
-
+knn = KNeighborsClassifier(n_neighbors = 12)
+knn.fit(xTrain, yTrain)
+print("test accuracy of knn with k=",12," is ",metrics.accuracy_score(yTest,  knn.predict(xTest)))
 
 '''
-Input will be test vector, training data, k from _chooseK function
-Output will be prediction and confidence
-'''
-def _knn(test, data, k):
-	#init
-	neighbours = []
-	kDistPack = []
-	
-	#for all traing vectors
-	for d in data:
-		dist = _distance(test,d)
-		#getting the k smallest neighbour
 
-		neighbours = _kSmallestSoFar(dist, d, neighbours, k)
-		
-	for n in neighbours:
-		kDistPack.append(n[1][0])
-	
-	#count of frequent appearence
-	count = Counter(kDistPack).most_common(k)
+knn = KNeighborsClassifier(n_neighbors = 12)
+knn.fit(xTrain, yTrain)
+print("Accuracy of knn with k=",12," is ",metrics.accuracy_score(yTest,  knn.predict(xTest))*100, "%")
 
-	if(len(count) == 1): return count[0][0], 1
-	#calculating confidence and percentage
-	confidence = int((count[0][1] / (count[0][1] + count[1][1]))*100)
-	#returns most common item and the first result and confidence
-	return count[0][0], confidence
+#predicting the values for the TestingResults from TesingData using TrainingData
+energyPred=knn.predict(xClassify)
+predDS = pd.DataFrame({'Prediction': energyPred})
+testDS = testDS.join(predDS)
+testDS.to_csv("TestingResults.txt", header=None, index=None)
 
-# euclidean distance calculation for vectors
-def _distance(v1,v2):
-	sum = float(0.0)
-	for i in range(0,23):
-		sum += math.pow( float(v1[i]) - float(v2[i]) , 2 )
-	return math.sqrt(sum)
-
-#getting k nearest neighbours so far
-def _kSmallestSoFar(dist: float, point, neighbours: [[float,str]], k: int) ->[[float,str]]:
-	if(len(neighbours) < min(10,k)) :
-		neighbours.append([ dist , point[-1]])
-		#always keeping a sorted list
-		neighbours.sort()
-		return neighbours
-	if(dist > neighbours[-1][0]): return neighbours
-	
-	neighbours.append([ dist , point[-1]])
-	#keping a list of sorted neighbours
-	neighbours.sort()
-	
-	return neighbours[:k]
-
-
-#testing the accuracy of knn
-def _accuracy_knn(training, k, outputFile=None):
-	f = None
-	if(not outputFile is None):
-		f = open(outputFile, 'w')
-	step = math.floor(len(training) / 10.0)
-	correct = 0
-	totalPercent = 0.0
-	
-	for i in range(0,len(training)):
-		if(i % step == 0):
-			print(str(i))
-		testElement = training[i]
-		modifiedTraining = training[:i] + training[i+1:]
-		prediction, confidence = courseClassifier(testElement, modifiedTraining, k)
-		if(int(prediction) == int(testElement[-1])):
-			correct = correct + 1
-			totalPercent = totalPercent + confidence
-		del(prediction)
-		del(confidence)
-		del(testElement)
-		del(modifiedTraining)
-	
-	#loop number and how many so far correct and number in traing data
-	line = str(i+1) + ": " + str(correct) +" out of "+ str(len(training)) + " correct, at " + str((totalPercent/(i+1)))+"% total confidence"
-	print(line)
-	if(not f is None):
-		f.write(line+'\n')
-		f.close()
-	return correct,(totalPercent/len(training))*100
-
-#detecting misses by KNN
-def _suspicion(festFectVector, data, k):
-	#setting suspicions baselines
-	high, mid, low, sus = 5,2,1,0
-	#checking if high suspicion
-	if((len(festFectVector) !=24) and (len(festFectVector) !=25)): sus = sus + high
-	PAR = np.sum([float(x) for x in festFectVector[0:23]])/24.0
-	if(PAR >4.762): sus = sus + high
-	for i in range(0,24):
-		if(i ==25 and not int(festFectVector[i])==0 and not int(festFectVector[i])==1): sus = sus + high
-		if(float(festFectVector[i]) <=0.0 and not i==25): sus = sus + high
-		if(float(festFectVector[i]) > 7.0): sus = sus + low
-		if(float(festFectVector[i]) > 8.0): sus = sus + mid
-		if(float(festFectVector[i]) > 10.0): sus = sus + high
-	if(len(data) < 9999): sus = sus + mid
-	if(len(data) < 5000): sus = sus + high
-	if(k <=0): sus = sus + high
-	return sus
-
-# Classifying a new festFectVector
-def courseClassifier(festFectVector, data, k):
-	#running knn
-	prediction, confidence = _knn(festFectVector, data, k)
-	
-	#ignore if confidence is below threshold
-	if(int(prediction) == 1 and int(confidence) <= getOHHOLD()):
-		prediction = 0
-		
-	#getting the value of suspicion
-	sus = _suspicion(festFectVector, data, k)
-	#if suspicion > threshold
-	if(sus >= getPETHOLD()):
-		#labeling data as abnormal
-		return 1, confidence + math.floor((100-confidence) /3)
-	return prediction, confidence
-
-#FN gets maxium value for energy from test data
-def _getMax(training):
-	max = 0.0
-	for i in range(0,len(training)):
-		for j in range(0,len(training[i])):
-			if(float(training[i][j]) > float(max)): max = training[i][j]
-	return max
-
-#for all testing data, courseClassifier data, label as 0 or 1
-def _categorize(testing, training, k, outputFile=None):
-	labeled = []
-	i = 1
-	for t in testing:
-		prediction, confidence = courseClassifier(t, training, k)
-		labeled.append(('line'+str(i), prediction, str(confidence)+'%',t))
-		line = "line "+str(i)+" = "+ str(prediction) +" @ "+ str(confidence) +'%'
-		print(line)
-		i = i +1
-	
-	if(not outputFile is None):
-		f = open(outputFile, 'w')
-		for t in [l for l in labeled]:
-			for p in t[-1]:
-				f.write(str(float(p))+',')
-			f.write(str(int(t[1]))+'\n')
-		f.close()
-	return labeled
-
-
-def main(args):
-	if(len(args)<=1):
-		print("No args")
-		exit(0)
-
-	training = _parseFile(args[1])
-	testing = _parseFile(args[2])
-	
-	if(training is None or training is [] or testing is None or testing is []):
-		print("bad input")
-		quit()
-
-	k = _chooseK(training)
-	
-	var = 0
-	try:
-		var = args[3]
-	except:
-		pass
-	
-	setOHHOLD(53)
-	
-	if(type(var) is str and os.path.splitext(var)[-1] == ".txt"):
-		labed = _categorize(testing, training, k, var)
-		abnormal = [int(l[0][4:]) for l in labed if int(l[1]) == 1]
-		print(str(len(abnormal))+" Abnormal results, on lines: "+str(abnormal))
-		quit()
-
-	var = int(var)
-	if(var == 0):
-		labed = _categorize(testing, training, k)
-		abnormal = [int(l[0][4:]) for l in labed if int(l[1]) == 1]
-		print(str(len(abnormal))+" Abnormal results, on lines: "+str(abnormal))
-	elif(var == 1):
-		_accuracy_knn(training, k, "baseAccurace-KNN.txt")
-	else:
-		pass
-		
-	
-if __name__ == '__main__':
-	main(sys.argv)
-	exit(0)
+print("Prediction of abnormal values are: ", energyPred)
---
+++ lpGeneratorPlot.py
@@ -0,0 +1,131 @@
+from pulp import LpMinimize, LpProblem, LpStatus, lpSum, LpVariable
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def readData():
+    #feeding user task from excel file shared
+    xlFile = pd.read_excel ('COMP3217CW2Input.xlsx', sheet_name = 'User & Task ID')
+    taskName = xlFile['User & Task ID'].tolist()
+    readyTime = xlFile['Ready Time'].tolist()
+    deadline = xlFile['Deadline'].tolist()
+    maxEnergyPerHour = xlFile['Maximum scheduled energy per hour'].tolist()
+    energyDemand = xlFile['Energy Demand'].tolist()
+    tasks = []
+    taskNames = []
+    
+    for k in range (len(readyTime)):
+        task = []
+        task.append(readyTime[k])
+        task.append(deadline[k])
+        task.append(maxEnergyPerHour[k])
+        task.append(energyDemand[k])
+        taskNames.append(taskName[k])
+        
+        tasks.append(task)
+              
+    #feeding predicted results from TestingResults.txt
+    testDS = pd.read_csv('TestingResults.txt', header=None)
+    yLabels = testDS[24].tolist()
+    testDS = testDS.drop(24, axis=1)
+    xData = testDS.values.tolist()
+    
+    return tasks, taskNames, xData, yLabels
+
+def createLPModel(tasks, taskNames):
+    '''function for scheduling problem'''
+
+    #required variables
+    taskVars = []
+    c = []
+    eq = []
+    
+    #creating a minimization LP model  
+    model = LpProblem(name="Scheduling-problem", sense=LpMinimize)
+    
+    #looping through the list of tasks
+    for ind, task in enumerate(tasks):
+        n = task[1] - task[0] + 1
+        tempList = []
+        #lopping between readyTime and deadline for ech tasks and creating variables with given constraints
+        for i in range(task[0], task[1] + 1):
+            x = LpVariable(name=taskNames[ind]+'_'+str(i), lowBound=0, upBound=task[2])
+            tempList.append(x)
+        taskVars.append(tempList)
+
+    #function for price minimization and appending to the model
+    for ind, task in enumerate(tasks):
+        for var in taskVars[ind]:
+            price = price_list[int(var.name.split('_')[2])]
+            c.append(price * var)
+    model += lpSum(c)
+            
+    #additional constraints for the model      
+    for ind, task in enumerate(tasks):
+        tempList = []
+        for var in taskVars[ind]:
+            tempList.append(var)
+        eq.append(tempList)
+        model += lpSum(tempList) == task[3]
+    
+    #feedin the model to caller for plotting
+    return model
+
+#Plotting the model against hourly usage in the community
+def plot(model, count):
+    hours = [str(x) for x in range(0, 24)]
+    pos = np.arange(len(hours))
+    users = ['user1', 'user2', 'user3', 'user4', 'user5']
+    color_list = ['midnightblue','mediumvioletred','mediumturquoise','gold','linen']
+    plot_list = []
+    to_plot = []
+    
+    #plot list for usage
+    for user in users:
+        tempList = []
+        for hour in hours:
+            hour_list_temp = []
+            task_count = 0
+            for var in model.variables():
+                if user == var.name.split('_')[0] and str(hour) == var.name.split('_')[2]:
+                    task_count += 1
+                    hour_list_temp.append(var.value())
+            tempList.append(sum(hour_list_temp))
+        plot_list.append(tempList)
+    
+    #stacked bar plot
+    plt.bar(pos,plot_list[0],color=color_list[0],edgecolor='black',bottom=0)
+    plt.bar(pos,plot_list[1],color=color_list[1],edgecolor='black',bottom=np.array(plot_list[0]))
+    plt.bar(pos,plot_list[2],color=color_list[2],edgecolor='black',bottom=np.array(plot_list[0])+np.array(plot_list[1]))
+    plt.bar(pos,plot_list[3],color=color_list[3],edgecolor='black',bottom=np.array(plot_list[0])+np.array(plot_list[1])+np.array(plot_list[2]))
+    plt.bar(pos,plot_list[4],color=color_list[4],edgecolor='black',bottom=np.array(plot_list[0])+np.array(plot_list[1])+np.array(plot_list[2])+np.array(plot_list[3]))
+
+    plt.xticks(pos, hours)
+    plt.xlabel('Hour')
+    plt.ylabel('Energy Usage (kW)')
+    plt.title('Energy Usage Per Hour For All Users\nDay %i'%count)
+    plt.legend(users,loc=0)
+    plt.savefig('Graph/Abnormal/Abnormal'+str(count)+'.png') ## for Abnormal graph
+    ##plt.savefig('Graph/Normal/Normal'+str(count)+'.png') ## for Normal graph
+
+    plt.clf()
+
+    return plot_list
+
+tasks, taskNames, xData, yLabels = readData()
+
+#scheduling and plotting abnormal dataset graphs
+#can be altered to plot normal as well
+for ind, price_list in enumerate(xData):
+    #label can be changed for normal or abnormal dataset depeneing upon the need
+    if yLabels[ind] == 1:
+        #creating model to solve and as input to plot function
+        model = createLPModel(tasks, taskNames)
+        answer = model.solve()
+        #print the model solution
+        print(answer)
+        #Plot the model
+        plot(model,ind+1)
+
+print("\n Abnormal plots can be found in the /graph/Abnormal folder")
+print("\n Normal plots can be found in the /graph/Normal folder")
